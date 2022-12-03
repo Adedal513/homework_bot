7fa972fc6eb5585cb8d6dfae64bc8c4d2699e7bd
Николай Мороз <adedalmoroz@gmail.com>
2022-12-03 20:38:44 +0300

    Review fixes

--- homework.py
+++ homework.py
@@ -1,10 +1,11 @@
 import os
+import sys
 
 import logging
 import telegram
 import time
 import requests
-from requests.exceptions import HTTPError
+from requests.exceptions import HTTPError, RequestException
 from dotenv import load_dotenv
 from http import HTTPStatus
 
@@ -20,7 +21,7 @@ RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-
+# К сожалению, при изменении имени переменной тесты не проходят :(
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
@@ -44,13 +45,11 @@ logger.addHandler(handler)
 
 def check_tokens() -> bool:
     """Проверка наличия всех необходимых переменных окружения."""
-    if not all([TELEGRAM_CHAT_ID, TELEGRAM_TOKEN]):
-        logger.critical('Ошибка импорта токенов Telegram.')
-
-        return False
-    elif not PRACTICUM_TOKEN:
-        raise SystemError('Ошибка импорта Auth-токена Домашки.')
-    return True
+    check_status = all([TELEGRAM_CHAT_ID, TELEGRAM_TOKEN, PRACTICUM_TOKEN])
+    if not check_status:
+        logger.critical('Ошибка импорта переменных окружения.')
+        raise SystemError('Ошибка импорта пременных окржения.')
+    return check_status
 
 
 def send_message(bot, message) -> None:
@@ -60,37 +59,27 @@ def send_message(bot, message) -> None:
         logger.debug(f'Сообщение успешно отправлено: {message}')
     except Exception as e:
         logger.error('Ошибка при отправке соообщения.')
-
         raise SystemError(f'Отправить сообщение не удалось: {e}')
 
 
 def get_api_answer(timestamp) -> dict:
     """Получение ответа от API."""
     params = {'from_date': timestamp}
-
+    logger.debug('Обращение к API отправлено.')
     try:
         response = requests.get(
             url=ENDPOINT,
             params=params,
             headers=HEADERS
         )
-    except Exception as e:
-        raise SystemError(f'Ошибка обращения к API, {e}')
-    else:
-        if response.status_code == HTTPStatus.OK:
-            logger.info('Ответ от API успешно получен')
-        elif 400 <= response.status_code < 500:
-            raise HTTPError(
-                '''Не удалось получить ответ от API:
-                ошибка на стороне клиента.''')
-        elif response.status_code >= 500:
-            raise HTTPError(
-                '''Не удалось получить ответ от API:
-                ошибка на стороне сервера.''')
 
-    response_dict = response.json()
-
-    return response_dict
+    except RequestException as e:
+        raise InnerException(e)
+    if response.status_code != HTTPStatus.OK:
+        error_msg = f'''Не удалось получить ответ от API.
+        Код ошибки: {response.status_code}'''
+        raise HTTPError(error_msg)
+    return response.json()
 
 
 def check_response(response) -> dict:
@@ -98,10 +87,9 @@ def check_response(response) -> dict:
     if not isinstance(response, dict):
         raise TypeError('Ответ API отличен от словаря')
 
-    try:
-        homeworks = response['homeworks']
-    except KeyError:
+    if 'homeworks' not in response:
         raise KeyError('Ошибка словаря по ключу homeworks')
+    homeworks = response['homeworks']
 
     if not isinstance(homeworks, list):
         raise TypeError('Тип ключа homeworks не list')
@@ -113,46 +101,58 @@ def parse_status(homework):
     """Проверка статуса проверки домашнего задания."""
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
+    if None in (homework_name, homework_status):
+        raise KeyError('Нет нужных ключей в ответе.')
 
-    if homework_name and homework_status:
-        if homework_status not in HOMEWORK_VERDICTS:
-            raise InnerException(
-                f'{homework_status} отсутствует в списке вердиктов.')
+    if homework_status not in HOMEWORK_VERDICTS:
+        raise InnerException(
+            f'{homework_status} отсутствует в списке вердиктов.')
 
-        verdict = HOMEWORK_VERDICTS.get(homework_status)
+    verdict = HOMEWORK_VERDICTS.get(homework_status)
 
-        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-
-    raise KeyError('Нет нужных ключей в ответе.')
+    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
-        return
+        sys.exit(1)
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time()) - RETRY_PERIOD
 
+    last_message, error_flag = '', False
+
     while True:
         try:
             response = get_api_answer(timestamp)
-            homework = check_response(response)
-
+            homeworks = check_response(response)
             logger.debug('Список работ получен.')
 
-            if len(homework):
-                status = parse_status(homework[0])
-                send_message(bot, status)
+            if homeworks:
+                message = parse_status(homeworks[0])
             else:
-                send_message(bot, 'Обновлений по домашкам пока нет :(')
+                message = 'Обновлений по домашкам пока нет :('
                 logger.debug('Заданий нет.')
+            '''
+            По идее, словарь и не требуется -- строка однозначно кодирует
+            состояние домашки (имя и статус проверки) --
+            если изменится хоть один параметр, то строка
+            тоже изменится, и сообщение отправится.
+            '''
+            if last_message != message:
+                send_message(bot, message)
+                last_message = message
+
+                error_flag = False
 
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            logger.error(f'{message}')
+            error_msg = f'Сбой в работе программы: {error}'
+            logger.error(f'{error_msg}')
 
-            send_message('Ошибка! Попробуйте позже.')
+            if not error_flag:
+                send_message('Ошибка! Попробуйте позже.')
+                error_flag = True
         finally:
             timestamp = int(time.time()) - RETRY_PERIOD
             time.sleep(RETRY_PERIOD)
